473a6a979d5a206fdb51be31c76384162c62ed07
Kareem <abdulkareemogedemgbe@outlook.com>
2026-06-13 11:34:47 +0100

    Create function to generate grid cells
    Create a function that generates an input number of grid cell squares
    per row and column. Add controller buttons and UI

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,1 +1,13 @@
-console.log("etch-a-sketch");
+function createGrid(size = 97) {
+  const gridContainer = document.querySelector(".js-grid-container");
+  const widthPercent = 100 / size;
+
+  for (let i = 0; i < size * size; i++) {
+    const grid = document.createElement("div");
+    grid.classList.add("grid-cell");
+    grid.style.width = widthPercent + "%";
+    gridContainer.appendChild(grid);
+  }
+}
+
+createGrid(64);
