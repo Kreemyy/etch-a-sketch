e0aeb52a9e512a349b77113174987cb7389175c4
Kareem <abdulkareemogedemgbe@outlook.com>
2026-06-14 17:32:12 +0100

    Add event listener on button to change grid size
    Add event listener to prompt user input for grid size. Keep prompting
    for valid input when input is outside the range 16 - 100 or user enters
    an empty string; with an infinite while loop. When user cancels prompt
    use default value for gridSize or the previous valid user input.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,7 @@
-function createGrid(size = 97) {
-  const gridContainer = document.querySelector(".js-grid-container");
-  const widthPercent = 100 / size;
+const gridContainer = document.querySelector(".js-grid-container");
 
+function createGrid(size = 16) {
+  const widthPercent = 100 / size;
   for (let i = 0; i < size * size; i++) {
     const grid = document.createElement("div");
     grid.classList.add("grid-cell");
@@ -10,4 +10,37 @@ function createGrid(size = 97) {
   }
 }
 
-createGrid(64);
+createGrid();
+let previousGridSize = 16;
+
+const sizeBtn = document.querySelector(".grid-size-btn");
+
+sizeBtn.addEventListener("click", () => {
+  let gridSize = prompt("Enter number of squares per side: 16 - 100", "16");
+
+  if (gridSize === null) {
+    gridContainer.textContent = "";
+    createGrid(previousGridSize);
+  } else if (+gridSize >= 16 && +gridSize <= 100) {
+    previousGridSize = gridSize;
+    gridContainer.textContent = "";
+    createGrid(previousGridSize);
+  } else {
+    while (+gridSize > 100 || +gridSize < 16 || gridSize === "") {
+      gridSize = prompt("Number must be between 16 - 100");
+      if (gridSize === null) {
+        gridContainer.textContent = "";
+        createGrid(previousGridSize);
+        break;
+      }
+    }
+  }
+});
+
+gridContainer.addEventListener("mousedown", (event) => {
+  if (event.target.classList.contains(grid - cell)) {
+  }
+});
+
+const clearSketchBtn = document.querySelector(".clear-sketch");
+clearSketchBtn.addEventListener("click", () => {});
